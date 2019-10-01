3108830a1ca34facf230a06fa87c9d6ce2c7cdbb
Chris <644191901@qq.com>
2019-10-01 16:02:30 +0800

    1.4.3

--- 04-JSPart/051-server4.js
+++ 04-JSPart/051-server4.js
@@ -2,34 +2,54 @@
 * @Author: Chris
 * @Date:   2019-10-01 11:41:18
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-10-01 14:19:22
+* @Last Modified time: 2019-10-01 14:06:51
 */
 /*
 	可以响应并返回文件
+	处理GET请求+POST请求
 */
 var http = require('http');
 var fs   = require('fs');
+var url = require('url');
+
 var server = http.createServer(function(req,res){
 	var urlStr = req.url;
+	console.log(req.method);
 	console.log('req.url:::',urlStr);
 	if(urlStr == '/favicon.ico'){
 		res.end('favicon.ico');
 	}
-	if(urlStr.search(/\?/) != -1){
+	if(req.method == 'POST'){
+		// res.end('post data...');
+		var body = '';
+		req.on('data',function(chunk){
+			body += chunk;
+		});
+		req.on('end',function(){
+			console.log('get post data::',body);
+			//根据数据做处理......
+			res.end(body);
+		})
+	}else if(req.method == 'GET'){
+		if(urlStr.search(/\?/) != -1){
 			var parm = url.parse(urlStr,true).query;
 			//根据数据做处理......
 			var json = JSON.stringify(parm);
 			res.end(json);
 		}
-	var filePath = './'+urlStr;
-	fs.readFile(filePath,function(err,data){
-		if(!err){
-			res.end(data);
-		}else{
-			res.statusCode = 404;
-			res.end('not found');
-		}
-	});
+		var filePath = './'+urlStr;
+		fs.readFile(filePath,function(err,data){
+			if(!err){
+				res.end(data);
+			}else{
+				res.statusCode = 404;
+				res.end('not found');
+			}
+		});
+	}else{
+		res.end('ok');
+	}
+	
 });
 /*
 局域网测试
