a794ca4b1e578e6d91f98d60e1be05e192c9c712
Chris <644191901@qq.com>
2019-10-13 00:23:25 +0800

    2.0.6

--- 12-react/js.js
+++ 12-react/js.js
@@ -2,7 +2,6 @@
 * @Author: Chris
 * @Date:   2019-10-11 17:49:18
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-10-12 23:15:44
+* @Last Modified time: 2019-10-13 00:23:05
 */
 // 
-alert(1);
