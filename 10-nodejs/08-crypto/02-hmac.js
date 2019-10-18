3ccb4fc15f75a4bbacd96e493c30fbd4d0e02fdc
Chris <644191901@qq.com>
2019-10-18 12:44:57 +0800

    2.2.5

--- 10-nodejs/08-crypto/02-hmac.js
+++ 10-nodejs/08-crypto/02-hmac.js
@@ -1,19 +1,15 @@
 /*
 * @Author: getgitch95
 * @Date:   2019-08-13 12:54:49
-* @Last Modified by:   getgitch95
-* @Last Modified time: 2019-08-13 12:59:37
+* @Last Modified by:   Chris
+* @Last Modified time: 2019-10-18 12:43:13
 */
 const crypto = require('crypto')
 const hmac = crypto.createHmac('sha356','yuiop67890')
-//md5 加密算法->不可逆
-//const hash = crypto.createHash('md5')
-//const hash = crypto.createHash('sha123')
-//const hash = crypto.createHash('323')
 
 
 //添加需要加密的明文
 hash.update('123456')
 
 //输出密文
-console.log(hash.digest('hex'))
+console.log(hmac.digest('hex'))
