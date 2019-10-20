036193672d317963fce3f3b0fbf7abb2d11f78f5
Chris <644191901@qq.com>
2019-10-20 22:17:46 +0800

    2.3.6-入口和出口

---
+++ 13-webpack/02-config/src/about.js
@@ -0,0 +1,7 @@
+/*
+* @Author: Chris
+* @Date:   2019-10-20 20:09:12
+* @Last Modified by:   Chris
+* @Last Modified time: 2019-10-20 22:12:36
+*/
+console.log('here is about.js file.........')
--- 13-webpack/02-config/webpack.config.js
+++ 13-webpack/02-config/webpack.config.js
@@ -2,19 +2,27 @@
 * @Author: Chris
 * @Date:   2019-10-20 21:00:11
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-10-20 21:47:04
+* @Last Modified time: 2019-10-20 22:15:19
 */
 const path = require('path')
 module.exports = {
 	//指定环境
 	mode:'development',
 	// mode:'production',
-	//入口
-	entry:'./src/index.js',
+	//单一入口
+	// entry:'./src/index.js',
+	// entry:{main:'./src/index.js'},
+	//多入口
+	entry:{
+		index:'./src/index.js',
+		about:'./src/about.js',
+		contact:'./src/contact.js'
+	},
 	//出口
 	output: {
 		//「入口分块(entry chunk)」的文件名模版
-		filename:'bundle.js',
+		// filename:'[name]-[chunkhash]-bundle.js',
+		filename:'[name]-[hash]-bundle.js',
 		//所有输出文件的目标路径
 		path:path.resolve(__dirname,'dist')
 	},
