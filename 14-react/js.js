d7d5dab2cffc1bd76cb49d36d28c487650c64e20
Chris <644191901@qq.com>
2019-10-21 21:03:14 +0800

    2.4.3

--- 14-react/js.js
+++ 14-react/js.js
@@ -2,6 +2,6 @@
 * @Author: Chris
 * @Date:   2019-10-11 17:49:18
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-10-21 21:02:30
+* @Last Modified time: 2019-10-21 21:03:04
 */
-console.log(js)
+alert('hello')
