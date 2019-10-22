604fb7f727a7c554f054b6917ab84aca6a782d1d
Chris <644191901@qq.com>
2019-10-22 23:16:11 +0800

    2.4.5

---
+++ 14-react/02-install/src/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Chris
+* @Date:   2019-10-22 21:25:21
+* @Last Modified by:   Chris
+* @Last Modified time: 2019-10-22 22:08:33
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<h1>hello React</h1>,document.getElementById('port'))
