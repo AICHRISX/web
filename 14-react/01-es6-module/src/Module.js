a8b98d9c2a7731c7a04453bdb691b3437d31ef98
Chris <644191901@qq.com>
2019-10-22 21:53:35 +0800

    2.4.4-es6模块化

---
+++ 14-react/01-es6-module/src/Module.js
@@ -0,0 +1,32 @@
+/*
+* @Author: Chris
+* @Date:   2019-10-22 21:27:55
+* @Last Modified by:   Chris
+* @Last Modified time: 2019-10-22 21:51:32
+*/
+//定义和导出模块
+console.log('here is a Module.js')
+/*
+export const a = 1
+export const b = 2
+*/
+/*
+const a = 1
+const b = 2
+export{a,b}
+*/
+/*
+const a = 1
+const b = 2
+export {a,b}
+*/
+/*
+const a = 1
+const b = 2
+export {a as a1,b as b1}
+*/
+const a = 1234
+const b = 7890
+export {b}
+
+export default a 
