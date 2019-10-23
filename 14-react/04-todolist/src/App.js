5387e08c5c9cefd580c9dc1cc417a4622429a06a
Chris <644191901@qq.com>
2019-10-23 10:55:52 +0800

    2.5.0-绑定事件

--- 14-react/04-todolist/src/App.js
+++ 14-react/04-todolist/src/App.js
@@ -2,18 +2,22 @@
 * @Author: Chris
 * @Date:   2019-10-23 09:40:06
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-10-23 10:37:39
+* @Last Modified time: 2019-10-23 10:52:49
 */
 import React,{ Component } from 'react'
 import "./App.css"
 
 class App extends Component{
+	handleClick(){
+		console.log(this)
+		console.log('btn click...')
+	}
 	render(){
 		return(
-		<div>		
+		<div className="App">		
 			<input />
-			<button className="btn">提交</button>
-			<ul style={{color:'red'}}>
+			<button onClick={this.handleClick.bind(this)}>提交</button>
+			<ul>
 				<li>吃饭</li>
 				<li>睡觉</li>
 				<li>敲代码</li>
