bdf78af8922cb6906688583728844fac68f4b0b6
Chris <644191901@qq.com>
2019-10-25 19:18:01 +0800

    2.6.4-redux-创建store

---
+++ 14-react/06-todolist-redux/src/store/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Chris
+* @Date:   2019-10-25 19:11:15
+* @Last Modified by:   Chris
+* @Last Modified time: 2019-10-25 19:14:48
+*/
+import { createStore } from 'redux'
+
+
+//创建store
+const store = createStore(()=>{})
+export default store
