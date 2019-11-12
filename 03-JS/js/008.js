b6035b45a5f3305aa3b42958c52950aa3f32a94b
Chris <644191901@qq.com>
2019-11-12 15:34:41 +0800

    js

--- 03-JS/js/008.js
+++ 03-JS/js/008.js
@@ -1,10 +1,9 @@
-
 function toChange(){
-	var oBox=document.getElementById('box');
-	oBox.style.width="300px";
-	oBox.style.height="300px";
-	oBox.style.background="yellow";
+	var oBox = document.getElementById('box');
+	oBox.style.width = '300px';
+	oBox.style.height = '300px';
+	oBox.style.background = 'yellow';
 
 	}
-	var oBox=document.getElementById('box');
+	var oBox = document.getElementById('box');
 	oBox.onclick = toChange;
