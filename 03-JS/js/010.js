50be1038af5ec45af9c04c52d941cdc1fd37869e
Chris <644191901@qq.com>
2019-11-12 16:34:26 +0800

    loading

---
+++ 03-JS/js/010.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: Chris
+ * @Date: 2019-11-12 16:26:03
+ * @LastEditTime: 2019-11-12 16:30:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /liyu(web)/03-JS/js/010.js
+ */
+window.onload = function (){
+    var oBox = document.getElementById('box');
+    function toRed(){
+        oBox.style.background = 'red';
+    }
+}
+
