e8f7facd34a88a79a8272085adec7c0850811032
Chris <644191901@qq.com>
2019-12-18 16:58:04 +0800

    update

--- 13-react/10-todolist-router/src/index.js
+++ 13-react/10-todolist-router/src/index.js
@@ -1,8 +1,8 @@
 /*
 * @Author: Chris
 * @Date:   2019-10-22 21:25:21
-* @Last Modified by:   Chris
-* @Last Modified time: 2019-10-26 15:07:59
+ * @Last Modified by: Chris:)
+ * @Last Modified time: 2019-12-18 16:57:16
 */
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -14,3 +14,7 @@ import store from './store'
 import App from './App.js'
 
 ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById('root'))
+// 热加载
+if(module.hot){
+    module.hot.accept();
+}
